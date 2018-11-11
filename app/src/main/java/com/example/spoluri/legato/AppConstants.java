b4c24323b9e43353195349a0ebea271324b16589
Sarat Poluri <sarat.poluri@gmail.com>
2018-11-11 01:47:46 -0700

    Youtube search implemented
    Signed-off-by: Sarat Poluri <sarat.poluri@gmail.com>

--- app/src/main/java/com/example/spoluri/legato/AppConstants.java
+++ app/src/main/java/com/example/spoluri/legato/AppConstants.java
@@ -5,4 +5,6 @@ public interface AppConstants
     public static final String YOUTUBE_API_KEY =  "AIzaSyDzgUdIUXe4dd9i_i7cH8whIbUSDgKZB_s";
     public static final int SEARCH_VIDEO = 1;
     public static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+    public static final String SEARCH_VIDEO_MSG = "Searching Videos";
+    public static final String DIALOG_TITLE = "Loading";
 }
--- app/src/main/java/com/example/spoluri/legato/YoutubeActivity.java
+++ app/src/main/java/com/example/spoluri/legato/YoutubeActivity.java
@@ -1,70 +1,108 @@
 package com.example.spoluri.legato;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.widget.Toast;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayer.Provider;
+import android.app.ProgressDialog;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import com.example.spoluri.legato.connections.*;
 
-public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener, ServerResponseListener {
+public class YoutubeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, ServerResponseListener {
 
     private static final int RECOVERY_REQUEST = 1;
     private YouTubePlayerView youTubeView;
+
+    private EditText mYtVideoEdt = null;
+    private Button mYtVideoBtn = null;
+    private ListView mYtVideoLsv = null;
+
+    private YtAdapter mYtAdapter = null;
+    private ServiceTask mYtServiceTask = null;
+    private ProgressDialog mLoadingDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
 
-        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-        youTubeView.initialize(AppConstants.YOUTUBE_API_KEY, this);
+        initializeViews();
+    }
 
-        // Service to search video
-        final String keyWord = "Jordan";
-        ServiceTask mYtServiceTask = new ServiceTask(AppConstants.SEARCH_VIDEO);
-        mYtServiceTask.setmServerResponseListener(this);
-        mYtServiceTask.execute(new String[]{keyWord});
+    /**
+     * Method that initializes views from the activity's content layout
+     */
+    private void initializeViews() {
+        mYtVideoEdt = (EditText) findViewById(R.id.yt_video_edt);
+        mYtVideoBtn = (Button) findViewById(R.id.yt_video_btn);
+        mYtVideoLsv = (ListView) findViewById(R.id.yt_video_lsv);
+
+        mYtVideoBtn.setOnClickListener(this);
+        mYtVideoLsv.setOnItemClickListener(this);
     }
 
     @Override
-    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
-        if (!wasRestored) {
-            player.cueVideo("PuWHYJF8Il8");
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.yt_video_btn:
+                final String keyWord = mYtVideoEdt.getText().toString().trim();
+                if (keyWord.length() > 0) {
+
+                    // Service to search video
+                    mYtServiceTask = new ServiceTask(SEARCH_VIDEO);
+                    mYtServiceTask.setmServerResponseListener(this);
+                    mYtServiceTask.execute(new String[]{keyWord});
+                }
+                break;
+            case R.id.video_item:
+                Toast.makeText(getApplicationContext(), "Jesus", Toast.LENGTH_SHORT).show();;
+                break;
         }
     }
 
     @Override
-    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
-        if (errorReason.isUserRecoverableError()) {
-            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
-        } else {
-            String error = String.format(getString(R.string.player_error), errorReason.toString());
-            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
-        }
-    }
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RECOVERY_REQUEST) {
-            // Retry initialization if user performed a recovery action
-            getYouTubePlayerProvider().initialize(AppConstants.YOUTUBE_API_KEY, this);
-        }
-    }
-
-    protected Provider getYouTubePlayerProvider() {
-        return youTubeView;
     }
 
     @Override
     public void prepareRequest(Object... objects) {
+        // Parse the response based upon type of request
+        Integer reqCode = (Integer) objects[0];
 
+        if(reqCode==null || reqCode == 0)
+            throw new NullPointerException("Request Code's value is Invalid.");
+        String dialogMsg = null;
+        switch (reqCode)
+        {
+            case SEARCH_VIDEO:
+                dialogMsg = SEARCH_VIDEO_MSG;
+                break;
+        }
+
+        if (mLoadingDialog != null && !mLoadingDialog.isShowing())
+            mLoadingDialog = ProgressDialog.show(this, DIALOG_TITLE, dialogMsg, true, true);
     }
 
     @Override
@@ -74,6 +112,29 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
 
     @Override
     public void completedRequest(Object... objects) {
+        // Dismiss the dialog
+        if (mLoadingDialog != null && mLoadingDialog.isShowing())
+            mLoadingDialog.dismiss();
 
+        // Parse the response based upon type of request
+        Integer reqCode = (Integer) objects[0];
+
+        if(reqCode==null || reqCode == 0)
+            throw new NullPointerException("Request Code's value is Invalid.");
+
+        switch (reqCode) {
+            case SEARCH_VIDEO:
+
+                if (mYtAdapter == null) {
+                    mYtAdapter = new YtAdapter(this);
+                    mYtAdapter.setmVideoList((List<SearchResult>) objects[1]);
+                    mYtVideoLsv.setAdapter(mYtAdapter);
+                } else {
+                    mYtAdapter.setmVideoList((List<SearchResult>) objects[1]);
+                    mYtAdapter.notifyDataSetChanged();
+                }
+
+                break;
+        }
     }
 }
