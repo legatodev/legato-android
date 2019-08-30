ef23faf2549a3f975d9a7385975fb8acd95557c6
Sarat Poluri <sarat.poluri@gmail.com>
2019-08-30 09:07:33 -0700

    Nullaway enabled. Errors fixed. TODO: cleanup warnings.

--- app/src/main/java/com/legato/music/youtube/ServiceTaskInterface.java
+++ app/src/main/java/com/legato/music/youtube/ServiceTaskInterface.java
@@ -1,11 +1,11 @@
 package com.legato.music.youtube;
 
-import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 
 interface ServiceTaskInterface {
-    HttpTransport transport = AndroidHttp.newCompatibleTransport();
+    HttpTransport transport = new NetHttpTransport();
     JsonFactory jsonFactory = new GsonFactory();
 }
