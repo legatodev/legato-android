d28e6e8581d780414146c0fe438a6cdc7163de6a
JustinTadlock <Justin-Tadlock@outlook.com>
2019-11-01 22:52:30 -0700

    Initial YouTubePlayerAdapter code.

---
+++ app/src/main/java/com/legato/music/youtube/YoutubePlayerAdapter.java
@@ -0,0 +1,150 @@
+package com.legato.music.youtube;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.legato.music.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class YoutubePlayerAdapter extends RecyclerView.Adapter<YoutubePlayerAdapter.ViewHolder> {
+
+    private LayoutInflater mInflator;
+    private String[] videoIds;
+    private Lifecycle lifecycle;
+
+    public YoutubePlayerAdapter(Context context, String[] videoIds, Lifecycle lifecycle)
+    {
+        mInflator = LayoutInflater.from(context);
+        this.videoIds = videoIds;
+        this.lifecycle = lifecycle;
+    }
+
+    @NonNull
+    @Override
+    public YoutubePlayerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView)mInflator
+                .inflate(R.layout.item_youtube_view, parent,false);
+        lifecycle.addObserver(youTubePlayerView);
+
+        return new ViewHolder(youTubePlayerView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        viewHolder.cueVideo(videoIds[position]);
+    }
+
+    public void setVideoIds(String[] videoIds)
+    {
+        this.videoIds = videoIds;
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoIds.length;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        final YoutubePlayerAdapter mAdapter;
+
+        private YouTubePlayerView youTubePlayerView;
+        private YouTubePlayer youTubePlayer;
+        private String currentVideoId;
+
+        ViewHolder(YouTubePlayerView playerView, YoutubePlayerAdapter adapter) {
+            super(playerView);
+
+            youTubePlayerView = playerView;
+            this.mAdapter = adapter;
+
+            youTubePlayer = new YoutubePlayer();
+            currentVideoId = "";
+
+            youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+                @Override
+                public void onReady(@NonNull YouTubePlayer initializedYouTubePlayer) {
+                    youTubePlayer = initializedYouTubePlayer;
+                    youTubePlayer.cueVideo(currentVideoId, 0);
+                }
+            });
+        }
+
+        void cueVideo(String videoId) {
+            currentVideoId = videoId;
+
+            if(youTubePlayer == null) {
+                return;
+            }
+
+            youTubePlayer.cueVideo(videoId, 0);
+        }
+    }
+
+    private static class YoutubePlayer implements YouTubePlayer {
+
+        public YoutubePlayer() {
+
+        }
+
+        @Override
+        public boolean addListener(@NotNull YouTubePlayerListener youTubePlayerListener) {
+            return false;
+        }
+
+        @Override
+        public void cueVideo(@NotNull String s, float v) {
+
+        }
+
+        @Override
+        public void loadVideo(@NotNull String s, float v) {
+
+        }
+
+        @Override
+        public void mute() {
+
+        }
+
+        @Override
+        public void pause() {
+
+        }
+
+        @Override
+        public void play() {
+
+        }
+
+        @Override
+        public boolean removeListener(@NotNull YouTubePlayerListener youTubePlayerListener) {
+            return false;
+        }
+
+        @Override
+        public void seekTo(float v) {
+
+        }
+
+        @Override
+        public void setVolume(int i) {
+
+        }
+
+        @Override
+        public void unMute() {
+
+        }
+    }
+}
