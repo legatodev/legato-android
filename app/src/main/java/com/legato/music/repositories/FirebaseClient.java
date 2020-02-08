fcb2937ea5efb7264490c8c48960397972d7e580
sarat poluri <sarat.poluri@gmail.com>
2020-02-08 16:15:57 +0000

    Issue #113: Email verification only for e-mail signup
    Approved-by: Justin Tadlock <justin.b.tadlock@gmail.com>

--- app/src/main/java/com/legato/music/repositories/FirebaseClient.java
+++ app/src/main/java/com/legato/music/repositories/FirebaseClient.java
@@ -2,6 +2,7 @@ package com.legato.music.repositories;
 
 import androidx.annotation.Nullable;
 
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -14,8 +15,8 @@ Any interaction with FireBase frame should be done by this class
 class FirebaseClient {
 
     @Nullable private  static FirebaseClient instance;
-    @Nullable private FirebaseDatabase mFirebaseDatabase;
-    @Nullable private FirebaseAuth mFirebaseAuth;
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mFirebaseAuth;
 
     public static FirebaseClient getInstance(){
         if(instance == null){
@@ -30,26 +31,20 @@ class FirebaseClient {
     }
 
     public DatabaseReference getChild(String pathString){
-        if(mFirebaseDatabase != null) {
             return mFirebaseDatabase.getReference().child(pathString);
-        }else{
-            return FirebaseDatabase.getInstance().getReference().child(pathString);
-        }
     }
 
     public Boolean isEmailVerified(){
-        if(mFirebaseAuth != null) {
+        if (EmailAuthProvider.PROVIDER_ID.equals(mFirebaseAuth.getCurrentUser().getProviderId())) {
             return mFirebaseAuth.getCurrentUser().isEmailVerified();
-        }else{
-            return FirebaseAuth.getInstance().getCurrentUser().isEmailVerified();
+        } else {
+            return true;
         }
     }
 
     public void sendEmailVerification(){
-        if(mFirebaseAuth != null) {
-            mFirebaseAuth.getCurrentUser().sendEmailVerification();
-        }else{
-            FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification();
+        if (EmailAuthProvider.PROVIDER_ID.equals(mFirebaseAuth.getCurrentUser().getProviderId())) {
+                mFirebaseAuth.getCurrentUser().sendEmailVerification();
         }
     }
 }
